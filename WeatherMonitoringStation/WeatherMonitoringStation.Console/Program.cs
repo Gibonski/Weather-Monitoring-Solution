d9c410e99870a5502d5b7f993cc667a65d5a8db5
Gibonski <144462526+Gibonski@users.noreply.github.com>
2024-04-10 22:02:17 +0000

    Console done

--- WeatherMonitoringStation/WeatherMonitoringStation.Console/Program.cs
+++ WeatherMonitoringStation/WeatherMonitoringStation.Console/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using WeatherMonitoringStation.Library;
+
+namespace WeatherMonitoringStation.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Weather Monitoring Station!");
+        }
+    }
+}
